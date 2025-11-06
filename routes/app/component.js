cd4b21aef52dc9224f05cdabd07f412697ccbfc3
PshsayhiXD <maiphucthinhaltx@gmail.com>
2025-11-06 17:11:20 +0700

    Fixed routes, fuck my dumbass

--- routes/app/component.js
+++ routes/app/component.js
@@ -2,10 +2,10 @@ import express from 'express';
 import paths from '../../utils/path.js';
 export default ({}) => {
   const Router = express.Router();
-  Router.get('/redirect/component.js', (req, res) => {
+  Router.get('/component.js', (req, res) => {
     res.sendFile(paths.html.component);
   });
-  Router.get('/redirect/component.test.js', (req, res) => {
+  Router.get('/component.test.js', (req, res) => {
     res.sendFile(paths.html.component_test);
   });
   return Router;
