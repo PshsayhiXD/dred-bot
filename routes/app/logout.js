96579a9e2953cf4a19558670175ee2cde5462090
PshsayhiXD <maiphucthinhaltx@gmail.com>
2025-11-07 11:07:56 +0700

    Updated routes

--- routes/app/logout.js
+++ routes/app/logout.js
@@ -1,16 +1,17 @@
-import express from 'express';
+import express from "express";
 
-export default ({ loadAllData, saveData }) => {
+export default ({ loadDataByAccountCookie, loadUsernameByAccountCookie, saveData }) => {
   const Router = express.Router();
-  Router.post('/logout', async (req, res) => {
-    const authToken = req.cookies['d_sess'];
-    const data = await loadAllData();
-    const user = Object.values(data).find((u) => u.account?.cookie === authToken);
-    if (user) user.account.cookie = null;
-    else return res.status(404).send('[+] Account cookie not found.')
+  Router.post("/logout", async (req, res) => {
+    const authToken = req.cookies["d_sess"];
+    const data = await loadDataByAccountCookie(authToken);
+    if (!data) return res.status(404).send("[+] Account cookie not found.");
+    const user = data.username || await loadUsernameByAccountCookie(authToken);
+    if (user) data.account.cookie = null;
+    else return res.status(404).send("[+] Account username not found.")
     await saveData(user, data);
-    try { res.clearCookie('d_sess'); } catch (e) {}
-    return res.status(200).send('[+] Logged out.');
+    res.clearCookie("d_sess", { httpOnly: true, secure: true, path: "/" });
+    return res.status(200).send("[+] Logged out.");
   });
   return Router;
 };
